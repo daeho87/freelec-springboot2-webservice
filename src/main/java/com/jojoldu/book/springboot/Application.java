66db58f5ce3683c950b664d784d843e6b2a2d964
daeho871 <daeho8710@naver.com>
2023-05-29 21:10:43 +0900

    jpa 실습

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] ags){
+        SpringApplication.run(Application.class, ags);
+    }
+}
