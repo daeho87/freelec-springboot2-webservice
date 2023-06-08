b616b3506968ca2352fb9b69120d51062e4221ee
daeho871 <daeho8710@naver.com>
2023-06-08 14:55:44 +0900

    구글로그인

---
+++ src/main/java/com/jojoldu/book/springboot/domain/user/User.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+}
