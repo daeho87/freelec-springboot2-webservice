f709629d336c862833d4e3f88c3e98df0d0849d7
daeho871 <daeho8710@naver.com>
2023-06-14 20:45:30 +0900

    최종커밋

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing//JPA Auditing 활성화
+//@EnableJpaAuditing//JPA Auditing 활성화 (@WebMvcTest 에서는 에러가 나기때문에 분리)
 @SpringBootApplication
 public class Application {
     public static void main(String[] ags){
